7a96131efc2dee4ad04a602d4c154d07a270d4d4
Dzikri Fauzi Ramdhani <dzikrifauziramdhani16@gmail.com>
2024-12-25 17:58:51 +0700

    add button to handle add list data

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -1,25 +1,39 @@
 package com.example.recyclerview
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var wordList: MutableList<String>
+    private lateinit var adapter: WordListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        val addButton: Button = findViewById(R.id.addButton)
 
-        // Data list
-        val wordList = mutableListOf<String>()
+        // Data
+        wordList = mutableListOf()
         for (i in 0 until 20) {
             wordList.add("Word $i")
         }
 
         // Call Adapter
-        recyclerView.adapter = WordListAdapter(wordList)
+        adapter = WordListAdapter(wordList)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+        // Click Add List
+        addButton.setOnClickListener {
+            val newWord = "Word ${wordList.size}"
+            wordList.add(newWord)
+            adapter.notifyItemInserted(wordList.size - 1)
+            recyclerView.scrollToPosition(wordList.size - 1)
+        }
     }
 }
